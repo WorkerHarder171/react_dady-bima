096c5935a4eadbd9e380a5031be1be7031612333
Overthinking171 <dadybima171@gmail.com>
2023-09-11 07:50:42 +0700

    Update TASK: 11_Introduction React

--- 11_Introduction React/Pratikum/my-app/src/App.js
+++ 11_Introduction React/Pratikum/my-app/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from 'react'
-
-
-const App = () => {
+function App() {
   return (
-    <div>My App</div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Im Learning React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
